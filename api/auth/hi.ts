a54f3f41f1c221ab189afe69850220c81054cdd4
jacobhsu <jacob.hsu.tw@gmail.com>
2025-01-03 20:03:19 +0800

    test: unAuthorizedResponse

--- api/auth/hi.ts
+++ api/auth/hi.ts
@@ -1,5 +1,6 @@
-import { useResponseSuccess } from "~/utils/response";
-export default defineEventHandler(() =>  {
+import { unAuthorizedResponse, useResponseSuccess } from "~/utils/response";
+export default defineEventHandler((event) => {
+  return unAuthorizedResponse(event);
   const codes = 1;
   return useResponseSuccess(codes);
 });
