3ccc17bf96c567b0a2de50b19b33b59803bf2937
jacobhsu <jacob.hsu.tw@gmail.com>
2025-01-06 15:01:14 +0800

    refactor: remove gh-pages /api

--- routes/[...].ts
+++ routes/[...].ts
@@ -1,15 +1,11 @@
 export default defineEventHandler(() => {
-const baseUrl = process.env.NODE_ENV === 'development' ?  '' : '/nitro-api';
 
 return `
 <h2>API Routes:</h2>
 <ul>
-<li><a href="${baseUrl}/api/hello">${baseUrl}/api/hello</a></li>
-<li><a href="${baseUrl}/api/hello/world">${baseUrl}/api/hello/world</a></li>
-<li><a href="${baseUrl}/api/test">${baseUrl}/api/test</a></li>
-<li><a href="${baseUrl}/api/auth/codes">/api/auth/codes</a></li>
-<li>https://nitro-api.vercel.app<a href="/server/hello">/server/hello</a></li>
-<li>https://nitro-api.vercel.app<a href="/server/test">/server/test</a></li>
+<li><a href="/server/hello">/server/hello</a></li>
+<li><a href="/server/hello/world">/server/hello/world</a></li>
+<li><a href="/server/test">/server/test</a></li>
 <li><a href="/auth/codes">/auth/codes</a></li>
 </ul>
 `;
